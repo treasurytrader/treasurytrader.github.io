35c9253216c34949ef82133b7156c2e9cca51f87
treasurytrader <rbooja@outlook.com>
2025-02-13 07:24:46 +0900

    Create bottom.js

---
+++ bottom.js
@@ -0,0 +1,26 @@
+// Open external links in new tabs and open internal links in current tabs
+var links    = document.getElementsByTagName("a");
+var thisHref = window.location.hostname;
+
+for(var i = 0; i < links.length; i++) {
+
+   templink = links[i].href;
+   a        = getLocation(templink);
+ 
+   if (a.hostname == thisHref) { // if the link is same with current page URL
+      links[i].removeAttribute("target");
+   } else {
+      links[i].target='_blank'; // if the link is same with current page URL
+   }
+}
+
+function getLocation(href) {
+
+   var location      = document.createElement("a");
+       location.href = href;
+
+   if (location.host == "") {
+      location.href = location.href;
+   }
+   return location;
+};
