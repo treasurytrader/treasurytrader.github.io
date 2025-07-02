0690a5568157f8e5ff1538eba54b22ff38d5bf7a
treasurytrader <rbooja@outlook.com>
2025-07-03 04:22:34 +0900

    Update head.js

--- head.js
+++ head.js
@@ -1,23 +1,28 @@
 (function() {
-    const my_js  = document.createElement('script');
-    my_js.type   = 'text/javascript';
-    my_js.async  = true;
-    my_js.src    = 'https://code.jquery.com/jquery-3.7.1.min.js';
-    // my_js.onload = function () {if(typeof my_example_init == "function"){my_example_init();} };
-    my_js.onload = function () {};
-    document.getElementsByTagName('head')[0].appendChild(my_js);
+   const js1  = document.createElement('script');
+   js1.type   = 'text/javascript';
+   js1.async  = true;
+   js1.src    = 'https://code.jquery.com/jquery-3.7.1.min.js';
+   // js1.onload = function () {if(typeof my_example_init == "function"){my_example_init();} };
+   js1.onload = function () {};
+   document.getElementsByTagName('head')[0].appendChild(js1);
 })();
 
-var meta = document.createElement('meta');
-meta.name = "description";
-meta.content = "해외선물과 닌자트레이더에 관한 것들";
-document.getElementsByTagName('head')[0].appendChild(meta);
+(function() {
+   const meta = document.createElement('meta');
+   meta.name = "description";
+   meta.content = "해외선물과 닌자트레이더에 관한 것들";
+   document.getElementsByTagName('head')[0].appendChild(meta);
+})();
 
-//현재 파일명+확장자 얻기        
-var thisfilefullname = document.URL.substring(document.URL.lastIndexOf('/') + 1, document.URL.length);
-//현재 파일명 얻기        
-var thisfilename = thisfilefullname.substring(thisfilefullname.lastIndexOf('.'), 0);
-document.title = decodeURI(thisfilename);
+(function() {
+   // 현재 파일명+확장자 얻기
+   const thisfilefullname = document.URL.substring(document.URL.lastIndexOf('/') + 1, document.URL.length);
+   // 현재 파일명 얻기
+   const thisfilename = thisfilefullname.substring(thisfilefullname.lastIndexOf('.'), 0);
+   document.title = decodeURI(thisfilename);
+})();
+
 /*
 URL 인코딩(encodeURI) / 디코딩(decodeURI)
 
