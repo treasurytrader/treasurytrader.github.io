e96ef4c08ad2282c65420443431736ce09ed567f
treasurytrader <rbooja@outlook.com>
2025-07-04 04:35:23 +0900

    Update footer.js

--- footer.js
+++ footer.js
@@ -48,33 +48,3 @@ else {
     document.onmouseup = reEnable
 }
 */
-// Open external links in new tabs and open internal links in current tabs
-var links    = document.getElementsByTagName("a");
-var thisHref = window.location.hostname;
-
-for(var i = 0; i < links.length; i++) {
-
-   templink = links[i].href;
-   a        = getLocation(templink);
- 
-   if (templink.includes(".pdf") || templink.includes(".jpg") || templink.includes(".png")) {
-      links[i].target='_blank';
-   }
-   else if (a.hostname == thisHref) { // if the link is same with current page URL
-      links[i].removeAttribute("target");
-   }
-   else {
-      links[i].target='_blank'; // if the link is same with current page URL
-   }
-}
-
-function getLocation(href) {
-
-   var location      = document.createElement("a");
-       location.href = href;
-
-   if (location.host == "") {
-      location.href = location.href;
-   }
-   return location;
-};
